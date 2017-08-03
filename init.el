81f40c2f9d9dceb55ad73227859ffba10955d863
Nidish96 <nidbid@gmail.com>
2017-08-03 23:49:22 +0530

    Cask and Pallet

--- init.el
+++ init.el
@@ -1,11 +1,11 @@
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (wheatgrass)))
- '(package-selected-packages (quote (package-build shut-up epl git commander f dash s))))
+ '(package-selected-packages (quote (package-build shut-up epl git commander f dash s)))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -13,9 +13,6 @@
  ;; If there is more than one, they won't work right.
  )
 
-(require 'package)
-(package-initialize)
-
 (require 'org)
 (org-babel-load-file
- (expand-file-name "config.org" user-emacs-directory))
+(expand-file-name "config.org" user-emacs-directory))
